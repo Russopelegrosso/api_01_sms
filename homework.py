98df9a69310f0eb64dac3a7f01caa730468206da
Арсен Дюбуа <arsenium1941@gmail.com>
2020-08-15 17:00:45 +0500

    review

--- homework.py
+++ homework.py
@@ -1,31 +1,44 @@
+import logging
 import time
 import os
 import requests
+from requests import RequestException
 
 from twilio.rest import Client
 from dotenv import load_dotenv
 
 load_dotenv()
 
+version_api = 5.122
+
+url = 'https://api.vk.com/method/users.get'
+
+acc_sid = os.getenv('account_sid')
+auth_token = os.getenv('auth_token')
+from_number = os.getenv('NUMBER_FROM')
+to_number = os.getenv('NUMBER_TO')
+
 
 def get_status(user_id):
     params = {
         'user_ids': user_id,
-        'v': 5.122,
+        'v': version_api,
         'fields': 'online',
         'access_token': os.getenv('vk_token'),
     }
-    status = requests.post('https://api.vk.com/method/users.get',
-                           params=params).json()['response']
-    return status[0]['online']
+    try:
+        status = requests.post(url, params=params).json()['response']
+        return status[0]['online']
+    except RequestException as err:
+        logging.debug(err, "'Error getting JSON'")
 
 
 def sms_sender(sms_text):
-    client = Client(os.getenv('account_sid'), os.getenv('auth_token'))
+    client = Client(acc_sid, auth_token)
     message = client.messages.create(
-        from_=os.getenv('NUMBER_FROM'),
+        from_=from_number,
         body=sms_text,
-        to=os.getenv('NUMBER_TO')
+        to=to_number
     )
     return message.sid
 
